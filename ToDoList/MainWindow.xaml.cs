78b25edcc4778c11c156f46fc877d183bd396dc5
Rostislav Dmitriev <usesurd@gmail.com>
2022-06-19 22:52:08 +0300

    upd

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+        
+    {
+        private BindingList<Model> _tododata;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _tododata = new BindingList<Model>()
+            {
+                new Model(){Task="test"}
+            };
+            dgToDo.ItemsSource= _tododata;
+        }
+    }
+}
---
+++ ToDoList/Models/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    internal class Model
+    {
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+		private bool _isDone;
+
+		public bool isDone
+		{
+			get { return _isDone; }
+			set { _isDone = value; }
+		}
+
+		private string _Task;
+
+		public string Task
+		{
+			get { return _Task; }
+			set { _Task = value; }
+		}
+
+
+	}
+}
